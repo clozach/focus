33a1210d968dbbc81c9c37a41b200df8793cef8d
Chris Lozac'h <clozach@gmail.com>
2019-10-15 14:41:27 -0700

    Several hours' worth of putzing.
    Usually I like to commit smaller changes and to detail what I've done, but for some reason I just got caught up in the Svelty fun. ¯\_(ツ)_/¯
    
    Anyway, quick overview of what you can do so far:
    
    - On start, hit Return to go fullscreen.
    - Once in fullscreen, typing edits the main text
      - Return to "accept" + (NYI) start the timer, which will default to 19 min.
    - ⌘-Delete to start typing from scratch (note: since I'm not using an Input, backspacing doesn't work. Should probably change to a textarea. 😶)

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,185 @@
 <script>
-	export let name;
+  import { onMount } from "svelte";
+  import Mousetrap from "mousetrap";
+  import { isUninked } from "./uninked-keypresses.js";
+
+  const debug = true;
+
+  const modes = {
+    fresh: "fresh",
+    idle: "idle",
+    enteringText: "enteringText",
+    countingDown: "countingDown",
+    enteringCountdownMinutes: "enteringCountdownMinutes"
+  };
+
+  const events = {
+    return: "return",
+    tab: "tab"
+  };
+
+  const placeholderContent =
+    "What are you going to do <strong>right now</strong>?";
+
+  // Simple state machine for `mode`
+  // fresh => enteringText <=> countingDown <=> enteringCountdownMinutes
+  let mode = "fresh";
+  let isUserGenerated = false;
+  let content = placeholderContent;
+  let countdownMinutes = 0;
+
+  $: Mousetrap.bind("return", () => returnHandler());
+  $: Mousetrap.bind("tab", () => tabHandler());
+
+  function returnHandler() {
+    mode = modeFrom(events.return, mode);
+  }
+
+  function tabHandler() {
+    mode = modeFrom(events.tab, mode);
+  }
+
+  function modeFrom(event, oldMode) {
+    if (event === events.return) {
+      switch (oldMode) {
+        case modes.fresh:
+          return modes.enteringText;
+
+        case modes.idle:
+          return modes.enteringText;
+
+        case modes.enteringText:
+          return modes.countingDown;
+
+        case modes.enteringCountdownMinutes:
+          return modes.countingDown;
+
+        case modes.countingDown:
+          return modes.enteringText;
+
+        default:
+          throw "Unknown mode";
+      }
+    } else if (event === events.tab) {
+      switch (oldMode) {
+        case modes.fresh:
+          return modes.fresh;
+
+        case modes.idle:
+          return modes.enteringText;
+
+        case modes.enteringText:
+          return modes.enteringCountdownMinutes;
+
+        case modes.enteringCountdownMinutes:
+          return modes.countingDown;
+
+        case modes.countingDown:
+          return modes.enteringCountdownMinutes;
+
+        default:
+          throw "Unknown mode";
+      }
+    }
+  }
+
+  const resetContent = () => {
+    content = placeholderContent;
+    mode = modes.enteringText;
+    isUserGenerated = false;
+  };
+
+  onMount(() => {
+    const fullscreen = () => {
+      // This take 2 hits of the [return] key…no idea why!
+      var docElm = document.documentElement;
+      if (docElm.requestFullscreen) {
+        docElm.requestFullscreen();
+      } else if (docElm.mozRequestFullScreen) {
+        docElm.mozRequestFullScreen();
+      } else if (docElm.webkitRequestFullScreen) {
+        docElm.webkitRequestFullScreen();
+      }
+      mode = modeFrom("return", mode);
+    };
+
+    Mousetrap.bind("return", fullscreen);
+    Mousetrap.bind("command+backspace", resetContent);
+    displayAndResetKeyPresses();
+  });
+
+  const keydownHandler = event => {
+    const key = event.key;
+
+    if (isUninked.test(key)) return;
+
+    if (mode === modes.enteringText) {
+      content = content === placeholderContent ? key : content + key;
+      isUserGenerated = true; // This is weak. Refactor, please!
+    } else if (mode === modes.enteringCountdownMinutes && /^[0-9]$/.test(key)) {
+      countdownMinutes = countdownMinutes + key;
+    }
+  };
+
+  const displayAndResetKeyPresses = () => {
+    document.addEventListener("keydown", keydownHandler);
+  };
+
+  let stopKeyPresses = () =>
+    document.removeEventListener("keydown", keydownHandler);
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  main {
+    font-size: 5rem;
+    text-align: center;
+    margin: 0 auto;
+    margin-top: 20vh;
+  }
+
+  .black {
+    color: black;
+  }
+
+  .gray {
+    color: lightgray;
+  }
+
+  .help {
+    position: absolute;
+    bottom: 3rem;
+    left: 3rem;
+    font-size: 2rem;
+    color: lightgray;
+  }
+
+  .debug {
+    position: absolute;
+    bottom: 3rem;
+    right: 3rem;
+    color: gray;
+    width: 50%;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<main class={isUserGenerated ? 'black' : 'gray'}>
+  {@html content}
+  <div class="help">Ctrl+Q to Quit</div>
+</main>
+
+{#if debug}
+  <div class="debug">
+    <div>
+      <strong>content:</strong>
+      {content}
+    </div>
+    <div>
+      <strong>mode:</strong>
+      {mode}
+    </div>
+    <div>
+      <strong>isUserGenerated:</strong>
+      {isUserGenerated}
+    </div>
+  </div>
+{/if}
---
+++ src/uninked-keypresses.js
@@ -0,0 +1,1 @@
+export const isUninked = /Shift|Control|Ctrl|Alt|Meta|Option|Command|Backspace|Tab|Enter|Return|Capslock|Esc|Escape|Space|Pageup|Pagedown|End|Home|Left|Up|Right|Down|Ins|Del|Plus/;
